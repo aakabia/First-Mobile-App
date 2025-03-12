9034fbb60411347d329943837548b6c2e9f29df9
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2025-03-12 18:16:32 -0400

    created orders detail page

--- First-Mobile-App/src/app/(shop)/orders/_layout.tsx
+++ First-Mobile-App/src/app/(shop)/orders/_layout.tsx
@@ -4,6 +4,7 @@ export default function OrdersLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown:false }} />
+      <Stack.Screen name="[slug]" options={{ headerShown:true }} />
     </Stack>
   );
 }
