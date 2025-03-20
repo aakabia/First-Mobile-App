d6c55e57b6a6a496928ee8d6fb55b504bd6a9109
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2025-03-20 12:29:59 -0500

    set up server side auth in next project

---
+++ rn-shop-admin/middleware.ts
@@ -0,0 +1,23 @@
+import { type NextRequest } from 'next/server'
+import { updateSession } from '@/utils/supabase/middleware'
+
+export async function middleware(request: NextRequest) {
+  return await updateSession(request)
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$|^/$).*)',
+  ],
+}
+
+// |^/$  this ensures that users have access to the home page. 
+// This code came from auth set up for server side auth with supabase in nextjs project 
+// This is auto ran on each route call that is not defined in matcher 
